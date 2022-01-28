ced5d37fdd44fb816274c0f93c21b5fb0910a11e
Luis Fernandez <luiferna2@WKMZTA6E6A41.global.publicisgroupe.net>
2022-01-28 16:25:22 -0600

    ngClass, ngStyle

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -10,6 +10,9 @@ export class UsersComponent implements OnInit {
   users: User[];
   showExtended:boolean=true;
   loaded:boolean=true;
+  enableAdd: boolean= true;
+  currentClasses={};
+  currentStyles={};
   constructor() {}
 
   ngOnInit(): void {
@@ -24,6 +27,7 @@ export class UsersComponent implements OnInit {
           city: "Alajuela",
           state: "San Ramon",
         },
+        image:'https://picsum.photos/300/300?random=1'
       },
       {
         firstName: "Luis",
@@ -34,14 +38,30 @@ export class UsersComponent implements OnInit {
           city: "Alajuela",
           state: "San Ramon",
         },
+        image:'https://picsum.photos/300/300?random=2',
+        isActive:true
       }
     ]
     this.addUser({
       firstName: "Roberto",
       lastName: "Fernandez",
     })
+    this.setCurrentClasses();
+    this.setCurrentStyles();
   }
   addUser(user:User){
     this.users.push(user)
+  }
+  setCurrentClasses(){
+    this.currentClasses={
+      'btn-success':this.enableAdd,
+      'big-text':this.showExtended
+    }
+  }
+  setCurrentStyles(){
+    this.currentStyles= {
+      'padding-top':this.showExtended ?'0':'40px',
+      'font-size':this.showExtended?'':'40px'
+    }
   }
 }
