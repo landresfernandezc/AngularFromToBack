a6920d318f8e719a38c301e492710d063d37cd21
Luis Fernandez <luiferna2@WKMZTA6E6A41.global.publicisgroupe.net>
2022-02-02 15:27:11 -0600

    restructurating

--- section8/src/app/app.module.ts
+++ section8/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { LogsComponent } from './components/logs/logs.component';
+import { LogFormComponent } from './components/log-form/log-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    LogsComponent,
+    LogFormComponent
   ],
   imports: [
     BrowserModule,
