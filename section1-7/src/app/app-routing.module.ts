4aae6184eb7eaf82bf8f598adb14a7079a4cf2d6
Luis Fernandez <luiferna2@WKMZTA6E6A41.global.publicisgroupe.net>
2022-02-02 15:16:41 -0600

    add not found component

--- section1-7/src/app/app-routing.module.ts
+++ section1-7/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 import { PostComponent } from './components/post/post.component';
 import { PostsComponent } from './components/posts/posts.component';
 import { UsersComponent } from './components/users/users.component';
@@ -10,6 +11,7 @@ const routes: Routes = [
   {path:'users',component:UsersComponent},
   {path:'posts',component:PostsComponent},
   {path:'post/:id',component:PostComponent},
+  {path:'**',component:NotFoundComponent},
 ];
 
 @NgModule({
